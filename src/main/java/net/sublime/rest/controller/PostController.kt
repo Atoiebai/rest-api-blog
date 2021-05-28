83b8f05183a7e0af5c59bd77cc11a3821f7a4a6a
Atoiebai <yusubov9999@mail.ru>
2021-05-28 11:41:58 +0500

    no changes

--- src/main/java/net/sublime/rest/controller/PostController.kt
+++ src/main/java/net/sublime/rest/controller/PostController.kt
@@ -7,9 +7,12 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
+import java.nio.file.Files
+import java.util.*
 
 @RestController
-@RequestMapping
+@RequestMapping("api/blog/posts")
 open class PostController(
     private val postService: PostService
 ) {
@@ -19,5 +22,17 @@ open class PostController(
         return if (postService.getAll().isEmpty())
             ResponseEntity(HttpStatus.NO_CONTENT)
         else ResponseEntity(postService.getAll(), HttpStatus.OK)
+    }
+
+
+
+    fun getLines(file: File , lines: Int , words: Int) {
+
+       val fileContent = Files.readAllLines(file.toPath())
+        fileContent.stream().skip(fileContent.size - lines.toLong())
+            .flatMap { line: String ->
+                val splitWords = line.split("\\s+".toRegex()).toTypedArray()
+                Arrays.stream(splitWords).skip(splitWords.size - words.toLong())
+            }.forEach(System.out:: println)
     }
 }
